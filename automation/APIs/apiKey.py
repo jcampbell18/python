56bbb01504d82bf45e216585c44e6f8ab4c04fb9
Jason Campbell <jcryuu@gmail.com>
2020-11-13 12:50:31 -0800

    updated so key is unseen

--- automation/APIs/apiKey.py
+++ automation/APIs/apiKey.py
@@ -1,9 +1,10 @@
 import requests
+import hiddenkeys
 
 baseUrl = 'http://api.openweathermap.org/data/2.5/weather'
 parameters = {
-    'id': '5811696',
-    'appid': '8bfe5d1cde91330c7c129bb3a505fae1'
+    'id': '5811696', # Spokane, WA
+    'appid': hiddenkeys.get_key()
 }
 response = requests.get(baseUrl, params=parameters)
 print(response.content)
